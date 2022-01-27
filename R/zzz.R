d3807cdfe6088d4de366d7ec4699471f30c3af78
peter.reader-harris@justice.gov.uk <peter.reader-harris@justice.gov.uk>
2022-01-27 21:02:00 +0000

    use closure instead of explicitly creating new environment

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-Rs3tools.env <- new.env()
-.onLoad <- function(libname, pkgname) {
-  # S3 service
-  Rs3tools.env$s3 <- NULL
-  # Will credentials need reauthentication (i.e. not handled by paws)?
-  Rs3tools.env$temporary_authentication <- FALSE
-  # Expiration of current credentials, if needed
-  Rs3tools.env$authentication_expiry <- NULL
-}
